f8c2d8bf0546fca2339bd826a35a4635bc4b0f41
harol1212 <harolvilchezespinoza@gmail.com>
2026-05-05 23:41:50 -0500

    Refactor script.py to remove commented stages

--- Lab3/scrip.py
+++ Lab3/scrip.py
@@ -1,16 +1,13 @@
-# Etapa 1
+# PARTE HAROL
 lista = [5, 8, 12, 3, 7, 9, 15, 2, 6, 10]
-# Etapa 2
 print("Elementos de la lista:")
 for i in lista:
     print(i)
-# Etapa 3
 nuevo_valor = int(input("Ingrese un nuevo valor para el tercer elemento: "))
 lista[2] = nuevo_valor
 print("\nLista actualizada:")
 for i in lista:
     print(i)
-# Etapa 4
 buscar = int(input("\nIngrese un número a buscar: "))
 if buscar in lista:
     print("El número sí existe en la lista.")

a0209f289761ad050cf79dde447b0a3329a5f9ec
harol1212 <harolvilchezespinoza@gmail.com>
2026-05-05 23:40:34 -0500

    Refactor script.py with stage comments

--- Lab3/scrip.py
+++ Lab3/scrip.py
@@ -1,22 +1,17 @@
-# Declarar lista de 10 elementos enteros
+# Etapa 1
 lista = [5, 8, 12, 3, 7, 9, 15, 2, 6, 10]
-
-# Recorrer e imprimir todos los elementos con un for
+# Etapa 2
 print("Elementos de la lista:")
 for i in lista:
     print(i)
-
-# Modificar el tercer elemento
+# Etapa 3
 nuevo_valor = int(input("Ingrese un nuevo valor para el tercer elemento: "))
 lista[2] = nuevo_valor
-
 print("\nLista actualizada:")
 for i in lista:
     print(i)
-
-# Buscar un número en la lista
+# Etapa 4
 buscar = int(input("\nIngrese un número a buscar: "))
-
 if buscar in lista:
     print("El número sí existe en la lista.")
 else:
